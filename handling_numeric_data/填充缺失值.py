a600e88a12a39c36a3ffcc2e7405af48e999afd8
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-05-20 01:42:39 +0800

    2020.5.20日常提交

--- handling_numeric_data/填充缺失值.py
+++ handling_numeric_data/填充缺失值.py
@@ -1,7 +1,8 @@
 import numpy as np
 from fancyimpute import KNN
-from sklearn.preprocessing import StandardScaler, Imputer
+from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_blobs
+from sklearn.impute import SimpleImputer
 
 # 创建模拟特征矩阵
 features, _ = make_blobs(n_samples=100, n_features=2, random_state=1)
@@ -15,11 +16,18 @@ true_value = standard_features[0, 0]
 standard_features[0, 0] = np.nan
 
 # 预测特征矩阵中的缺失值
-features_knn_imputed = KNN(k=5, verbose=0).complete(standard_features)
+features_knn_imputed = KNN(k=5, verbose=False).fit_transform(standard_features)
 
 # 对比真实值和填充值
 print("True Value: ", true_value)
 print("Imputed Value：", features_knn_imputed[0, 0])
 
+# 创建填充器
+mean_imputer = SimpleImputer(strategy="mean")
 
+# 填充缺失值
+features_mean_imputed = mean_imputer.fit_transform(features)
 
+# 对比真实值和填充值
+print("True Value: ", true_value)
+print("Imputed Value：", features_mean_imputed[0, 0])
